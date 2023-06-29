4d5c5a5f9b41dbec610400165e137d86ce2276ee
John Temiloluwa Ajala <treformers@gmail.com>
2023-06-28 22:44:37 -0500

    Update QuizController.php

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -7,29 +7,36 @@ use App\Models\Question;
 
 class QuizController extends Controller
 {
+    public $questions;
+    
+    public function __construct()
+    {
+        $this->questions = Question::all();
+    }
+
     //
     public function index()
     {
-        $questions = Question::all();
+        $questions = $this->questions; // Question::all();
         return view('quiz', compact('questions'));
     }
 
     public function score(Request $request)
     {
         $answers = $request->input('answers');
-        $questions = Question::all();
+        $questions = $this->questions; // Question::all();
         $score = 0;
 
         foreach ($questions as $question) {
             // $answerKey = 'answer_' . $question->id;
 
-            if ($question->answer == $answers[1]) {
+            if ($question->answer === $answers[$question->id]) {
                 $score++;
             }
 
             // Save the user's answer in the database
             // $question->user_answer = $answers[$answerKey][0];
-            $question->user_answer = $answers[1];
+            $question->user_answer = $answers[$question->id];
             // $question->save();
         }
 
